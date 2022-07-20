329e80629a8cb4acce782bd24f9fedb5c41ae664
Абдурахман Усманов <ausmanov706@gmail.com>
2022-07-20 21:11:06 +0300

    Доработал файл aboutTheDeveloper.c

--- aboutTheDeveloper.c
+++ aboutTheDeveloper.c
@@ -7,8 +7,9 @@
 void aboutDev()
 {
     printf("\n\n\nDeveloping of programm for working with list of yacht attributed to club");
-    printf("\nDeveloped by student of ICTMS 1-5");
-    printf("\nUsmanov Abdurakhman Khasmagamedovich, 2022 year");
+    printf("\n========================================================================");
+    printf("\nMy name is Abdurakhman Usmanov");
+    printf("\nI am a student of ICTMS 1-5");
     printf("\n\nwrite EXIT to escape: ");
     char s[10];
     scanf("%s",s);
